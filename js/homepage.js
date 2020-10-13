15b62ef13077c6388a8cc4eebffa24d1bf3e9296
윤정민(종이상자) <lego37yoon@outlook.com>
2020-10-13 18:51:14 +0900

    alpha 0.0.2
    - Bootstrap 제거
    - Pure CSS 추가
    - 각종 UI 커스텀 적용: 세부 페이지 UI 변경

--- js/homepage.js
+++ js/homepage.js
@@ -1,10 +1,15 @@
 /* 공통 */
-//Bootstrap 들여오기
-var cssOne = '<link rel="stylesheet" type="text/css" href="';
-var cssResourceOne = browser.extension.getURL("../css/bootstrap.min.css");
-var cssTwo = '">'
-var myCSS = cssOne.concat(cssResourceOne, cssTwo);
-//document.head.innerHTML = myCSS;
+//PureCSS 들여오기
+var pureCore = document.createElement("link");
+pureCore.rel = "stylesheet";
+pureCore.type = "text/css";
+pureCore.href = browser.extension.getURL("../css/menus-core.css");
+var pureDropdown = document.createElement("link");
+pureDropdown.rel = "stylesheet";
+pureDropdown.type = "text/css";
+pureDropdown.href = browser.extension.getURL("../css/menus-dropdown.css");
+document.head.appendChild(pureCore);
+document.head.appendChild(pureDropdown);
 
 //995px 제한 해제
 let layout995 = document.getElementsByClassName('lo_995');
@@ -21,29 +26,104 @@ mainLogo.parentNode.removeAttribute("class");
 mainLogo.parentNode.removeAttribute("style");
 mainLogo.parentNode.setAttribute('style', 'margin: 0px 20px 20px 20px;');
 //상단 녹색 제거
-document.getElementById("Main_Contents").style.backgroundColor = "white";
-document.getElementById("Top_Main").style.backgroundColor = "white";
+if (document.getElementById("Main_Contents") != null) {
+    document.getElementById("Main_Contents").style.backgroundColor = "white";
+}
+if (document.getElementById("Top_Main") != null) {
+    document.getElementById("Top_Main").style.backgroundColor = "white";
+} else {
+    document.getElementById("Top").setAttribute('id', 'myTop');
+    document.getElementById("myTop").setAttribute('style', 'width: 100%;height: 205px;');
+}
 //상단 컨텐츠 너비 제한 제거 & 나머지 녹색 제거
-document.querySelector("div[class*='jwxe_root Top_Menu']").removeAttribute("style");
+let schTopMenu = document.querySelector("div[class*='jwxe_root Top_Menu']");
+schTopMenu.removeAttribute("style");
+schTopMenu.style.backgroundColor = "white";
+schTopMenu.parentNode.style.width = "";
 //로그인 버튼 텍스트로
-document.querySelector("a[href*='/_custom/sch/oAuth/login.jsp']").setAttribute('id', 'loginBtn');
+if (document.querySelector("a[href*='/_custom/sch/oAuth/login.jsp']") === null) {
+    document.querySelector("a[href*='/_common/login/proc/log_out.jsp']").setAttribute('id', 'loginBtn');
+} else {
+    document.querySelector("a[href*='/_custom/sch/oAuth/login.jsp']").setAttribute('id', 'loginBtn');
+}
+
 let headerLogin = document.getElementById("loginBtn");
 headerLogin.style.width = "";
-headerLogin.innerHTML = "로그인";
-headerLogin.parentNode.setAttribute('style', 'position: relative; float:left;')
+if(document.querySelector("img[src*='/sch/_res/img/ttmenu_logout.gif']") === null) {
+    headerLogin.innerHTML = "로그인 |";
+} else {
+    headerLogin.innerHTML = "로그아웃 |";
+}
+headerLogin.parentNode.setAttribute('style', 'position: relative;float: left;margin-left: 4px;');
 headerLogin.parentNode.parentNode.style.width = "";
+//상단 메뉴 | 제거
+let container = document.querySelector("#login_list");
+let loginBar = container.querySelectorAll("img[src*='https://homepage.sch.ac.kr/sch/_res/img/ttmenu_bar.gif']");
+loginBar.forEach(function(userItem) {
+    userItem.remove();
+})
+//홈 버튼 텍스트로
+let headerHome = document.getElementById('jwxe_1300019677522');
+headerHome.style.width = "";
+headerHome.innerHTML = "홈 |";
+headerHome.parentNode.setAttribute('style', 'position: relative;float: left;margin-left: 5px;');
+//영문 버튼 텍스트로
+document.querySelector("a[href*='/english']").setAttribute('id', 'EnglishBtn');
+let headerEnglish = document.getElementById('EnglishBtn');
+headerEnglish.style.width = "";
+headerEnglish.innerHTML = "English";
+headerEnglish.parentNode.setAttribute('style', 'position: relative;float: left;margin-left: 5px; margin-right: 5px;');
 
+
+//메인 이미지 정렬 및 색상 채우기
+let mainBannerNum = 0;
+for (let i = 1; i < 7; i++) {
+    const queryStr = "img[src*='/sch/_res/img/sub_visual_0" + i + ".gif']"
+    if (document.querySelector(queryStr) != null) {
+        let mainBannerImg = document.querySelector(queryStr);
+        mainBannerImg.parentNode.parentNode.removeAttribute("width");
+        mainBannerImg.style.float = "right";
+        switch(i) {
+            case 1:
+                mainBannerImg.style.backgroundColor = "#284797";
+                break;
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            default:
+                mainBannerImg.style.backgroundColor = "#294A9B";
+                break;
+        }
+    }
+}
 
 /* 메인 컨텐츠*/
 //하단 녹색 제거
 document.getElementById("jwxe_main_content").style.backgroundColor = "white";
 document.body.style.backgroundColor = "white";
-// 순천향인 버튼 제거
-document.getElementById("jwxe_1297997945786").innerHTML = "순천향인"
-//교직원 버튼 제거
-document.getElementById("jwxe_1297997969913").innerHTML = "교직원"
-//방문인 버튼 제거
-document.getElementById("jwxe_1298002463910").innerHTML = "방문인"
+try {
+    // 순천향인 버튼 제거
+    document.getElementById("jwxe_1297997945786").innerHTML = "순천향인"
+    //교직원 버튼 제거
+    document.getElementById("jwxe_1297997969913").innerHTML = "교직원"
+    //방문인 버튼 제거
+    document.getElementById("jwxe_1298002463910").innerHTML = "방문인"
+    //각종 공지 배경 제거
+    let notices = document.getElementsByClassName("jwxe_div");
+    notices[0].style.background = "";
+    notices[1].style.background = "";
+    notices[3].style.background = "";
+    let noticeArticles = document.querySelectorAll("a[style*='z-index: 101;']");
+    for (let i = 0; i < noticeArticles.length; i++) {
+        noticeArticles[i].style.color = "black";
+    }
+    //공지 제목들 이미지 제거
+    
+} catch (error) {
+    
+}
 
 
 /* 하단 푸터 영역 */
@@ -65,31 +145,46 @@ addressStyle.removeAttribute("style");
 addressStyle.removeAttribute("class");
 addressStyle.innerHTML = "<p>[31538] 충남 아산시 신창면 순천향로 22 TEL.041-530-1114 FAX.041-542-4615<br>COPYRIGHT BY 2020 SOONCHUNHYANG UNIVERSITY. ALL RIGHT RESERVED</p>"; //텍스트 삽입 + 통합
 //저작권 div 삭제
-document.querySelector("div[style*='left:140px; top:15px; height:20px; width:392px;']").setAttribute('id', 'footerCopyright'); //ID 생성
+if(document.querySelector("img[src='https://homepage.sch.ac.kr/sch/_res/img/copyright.gif']") != null) {
+    document.querySelector("img[src='https://homepage.sch.ac.kr/sch/_res/img/copyright.gif']").setAttribute('id', 'footerCopyright');
+} else {
+    document.querySelector("img[src='/sch/_res/img/copyright.gif']").setAttribute('id', 'footerCopyright');
+} //ID생성
 let copyright = document.getElementById("footerCopyright");
 copyright.remove();
-//policy, infoLink, remoteService 정렬
-document.querySelector("div[style*='left:530px;top:3px;width:270px;height:30px;']").removeAttribute("style");
-document.querySelector("ul[class*='fmenu']").setAttribute('style', 'display: inline;');
-document.querySelector("ul[class*='fmenu']").parentNode.removeAttribute("class");
-//개인정보 취급방침 대체
-document.querySelector("a[href*='/sch/pop/pop_privacy_policy.jsp']").setAttribute('id', 'footerPolicy'); //ID 생성
-let policy = document.getElementById('footerPolicy');
-policy.innerHTML = "개인정보 취급방침"; //텍스트 삽입
-policy.removeAttribute("class");
-policy.removeAttribute("style");
-policy.setAttribute('style', 'margin-left: 10px; color: cornflowerblue;'); //색상 설정
-//대학 정보 공시 대체
-document.querySelector("a[href*='http://www.academyinfo.go.kr/pubinfo/pubinfo1600/doInit.do?schlId=0000142']").setAttribute('id', 'footerInfo'); //ID 생성
-let infoLink = document.getElementById("footerInfo");
-infoLink.innerHTML = "대학정보공시"
-//원격 지원 서비스 대체
-document.querySelector("a[href*='http://1.sch.ac.kr']").setAttribute('id', 'footerRemote'); //ID 생성
-let remoteService = document.getElementById("footerRemote");
-remoteService.innerHTML = "원격지원서비스"
-//바로가기 조정
-document.querySelector("div[style*='left:800px;width:190px;height:30px;']").setAttribute('id', 'footerGo'); //ID 생성
-let toGo = document.getElementById('footerGo');
-toGo.removeAttribute("class"); //기존 속성값 제거
-toGo.removeAttribute("style");
-toGo.setAttribute('style', 'margin-right: 10px; float: right;'); //오른쪽으로 정렬, 여백 10px
+
+try {
+    //policy, infoLink, remoteService 정렬
+    document.querySelector("div[style*='left:530px;top:3px;width:270px;height:30px;']").removeAttribute("style");
+    document.querySelector("ul[class*='fmenu']").setAttribute('style', 'display: inline;');
+    document.querySelector("ul[class*='fmenu']").parentNode.removeAttribute("class");
+    //개인정보 취급방침 대체
+    document.querySelector("a[href*='/sch/pop/pop_privacy_policy.jsp']").setAttribute('id', 'footerPolicy'); //ID 생성
+    let policy = document.getElementById('footerPolicy');
+    policy.innerHTML = "개인정보 취급방침"; //텍스트 삽입
+    policy.removeAttribute("class");
+    policy.removeAttribute("style");
+    policy.setAttribute('style', 'margin-left: 10px; color: cornflowerblue;'); //색상 설정
+    //대학 정보 공시 대체
+    document.querySelector("a[href*='http://www.academyinfo.go.kr/pubinfo/pubinfo1600/doInit.do?schlId=0000142']").setAttribute('id', 'footerInfo'); //ID 생성
+    let infoLink = document.getElementById("footerInfo");
+    infoLink.innerHTML = "대학정보공시"
+    //원격 지원 서비스 대체
+    document.querySelector("a[href*='http://1.sch.ac.kr']").setAttribute('id', 'footerRemote'); //ID 생성
+    let remoteService = document.getElementById("footerRemote");
+    remoteService.innerHTML = "원격지원서비스"
+    //바로가기 조정
+    document.querySelector("div[style*='left:800px;width:190px;height:30px;']").setAttribute('id', 'footerGo'); //ID 생성
+    let toGo = document.getElementById('footerGo');
+    toGo.removeAttribute("class"); //기존 속성값 제거
+    toGo.removeAttribute("style");
+    toGo.setAttribute('style', 'margin-right: 10px; float: right;'); //오른쪽으로 정렬, 여백 10px   
+} catch (error) {
+    
+}
+try {
+    let footerQR = document.getElementsByClassName("jwxe_include_item");
+    footerQR[0].setAttribute('style', 'right: 66%; bottom: -0');
+} catch (error) {
+    
+}
